94eff9262efdf5e7e3d64093b9111759fba9e31d
ronohoscar <kipngetichronohoscar@gmail.com>
2025-11-10 22:08:34 +0300

    Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2);
+let count = args.length;
+
+if (count === 0){
+    console.log("No argument");
+}
+else if (count === 1){
+    console.log("Argument found");
+}
+else{
+    console.log("Arguments found");
+}
