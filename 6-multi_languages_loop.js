68a14ac9e65897dc4d3d869c6a54acfe1955efb5
ronohoscar <kipngetichronohoscar@gmail.com>
2025-11-11 22:57:11 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for(let i= 0; i<languages.length; i++){
-    console.log(languages[i]);
+
+for (const lang of languages) {
+  console.log(lang);
 }
