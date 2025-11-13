dc333f91a790a979fe1ce32f782c49879cc466be
ronohoscar <kipngetichronohoscar@gmail.com>
2025-11-13 22:32:16 +0300

    Update square

--- 8-square.js
+++ 8-square.js
@@ -1,17 +1,12 @@
 const arg = process.argv[2];
 const size = Number(arg);
 
-if (isNaN(size)){
-console.log("Missing size");
+if (isNaN(size)) {
+  console.log('Missing size');
+} else if (size > 0) {
+  let i = 0;
+  while (i < size) {
+    console.log('X'.repeat(size));
+    i++;
+  }
 }
-else if(size>0){
-    const row = "X".repeat(size);
-    for(let i = 0; i<size; i++){
-        console.log(row)
-
-    };
-
-};
-
-
-
