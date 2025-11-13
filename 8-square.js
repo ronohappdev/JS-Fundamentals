abce10dda5ac75ac57fc94f166a4228e1c4eb585
ronohoscar <kipngetichronohoscar@gmail.com>
2025-11-13 22:23:46 +0300

    Square

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const arg = process.argv[2];
+const size = Number(arg);
+
+if (isNaN(size)){
+console.log("Missing size");
+}
+else if(size>0){
+    const row = "X".repeat(size);
+    for(let i = 0; i<size; i++){
+        console.log(row)
+
+    };
+
+};
+
+
